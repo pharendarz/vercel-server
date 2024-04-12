390013255dc12baeb6c2dc0fbc6f0e0571871d25
pharendarz <p.harendarz@gmail.com>
2024-04-12 16:45:39 +0200

    first commit

---
+++ api/index.ts
@@ -0,0 +1,43 @@
+import express from "express";
+import { createServer } from "http";
+
+const expressApp = express();
+const server = createServer(expressApp);
+
+expressApp.use((req, res, next) => {
+  res.header(
+    "Access-Control-Allow-Headers",
+    "x-access-token, Origin, Content-Type, Accept"
+  );
+  next();
+});
+
+expressApp.use((req, res, next) => {
+  // Website you wish to allow to connect
+  res.setHeader("Access-Control-Allow-Origin", "*"); // || 'port'
+  // Request methods you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+  // Request headers you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type"
+  );
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  // @ts-ignore
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  // Pass to next layer of middleware
+  next();
+});
+
+expressApp.get("/", (req, res) => {
+  res.send({ app: "vercel-server" });
+});
+
+server.listen(4000, () => {
+  // tslint:disable-next-line:no-console
+  console.log("[server] started on port " + 4000);
+});
