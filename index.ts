4ff5284ee0110103932b61e8ce4dd5a24a19f00e
pharendarz <p.harendarz@gmail.com>
2024-04-15 11:54:02 +0200

    cors

--- index.ts
+++ index.ts
@@ -31,7 +31,7 @@ expressApp.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Credentials", true);
   res.setHeader("Access-Control-Allow-Origin", "*");
   // another common pattern
-  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
+  res.setHeader("Access-Control-Allow-Origin", req.headers.origin);
   res.setHeader(
     "Access-Control-Allow-Methods",
     "GET,OPTIONS,PATCH,DELETE,POST,PUT"
